60f0a1f41f8d8b1aaf6c562a39a543f59ff8a120
bestoon <148741679+vestoon@users.noreply.github.com>
2024-07-23 22:48:38 +0900

    Title: 두 사람, Time: 100ms, Memory: 24MB, Status: Passed - CodeTree

--- 240723/두 사람/two-person.py
+++ 240723/두 사람/two-person.py
@@ -1,6 +1,6 @@
 for _ in range(2):
-    a, g = map(int, input().split())
-    if a >=19 and g=='M':
+    a, g =input().split()
+    if int(a) >=19 and g=='M':
         print(1)
         break
 else:
