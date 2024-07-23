c3c38ddf500065ce1a733eab96574c3e260fef62
bestoon <148741679+vestoon@users.noreply.github.com>
2024-07-23 21:56:09 +0900

    Title: 비교 연산, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240723/비교 연산/comparison-operator.py
@@ -0,0 +1,8 @@
+a, b = map(int, input())
+
+print(1 if a >= b else 0)
+print(1 if a > b else 0)
+print(1 if b >= a else 0)
+print(1 if b > a else 0)
+print(1 if a == b else 0)
+print(1 if a != b else 0)
