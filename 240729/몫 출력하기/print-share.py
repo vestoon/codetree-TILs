d258a4ba3c1e298d328124a49a3e5eaedcfec1c7
bestoon <148741679+vestoon@users.noreply.github.com>
2024-07-29 23:59:13 +0900

    Title: 몫 출력하기, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240729/몫 출력하기/print-share.py
+++ 240729/몫 출력하기/print-share.py
@@ -1,5 +1,9 @@
-for _ in range(3):
+cnt = 3
+
+while True:
     n = int(input())
-    if n%2:
-        continue
-    print(n//2)
+    if n%2 == 0:
+        print(n//2)
+        cnt -= 1
+        if not cnt:
+            break
