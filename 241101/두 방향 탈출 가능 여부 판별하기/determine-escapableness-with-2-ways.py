8575be9bf2388928f9e4e64a8b503d7e0166262b
bestoon <148741679+vestoon@users.noreply.github.com>
2024-11-01 20:20:35 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 241101/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 241101/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -3,17 +3,19 @@ input = sys.stdin.readline
 
 n , m = map(int, input().split())
 grid = [list(map(int, input().split())) for x in range(n)]
+visited = [[False for x in range(m)] for y in range(n)]
 
 
 def dfs(i, j):
+    visited[i][j] = True
     if i == n-1 and j == m-1:
         return True
 
     if j<m-1 and grid[i][j+1]:
-        if dfs(i, j+1):
+        if not visited[i][j+1] and dfs(i, j+1):
             return True
     if i<n-1 and grid[i+1][j]:
-        if dfs(i+1, j):
+        if not visited[i+1][j] and dfs(i+1, j):
             return True
     
     return False
