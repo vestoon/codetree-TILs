e57976cd098303ef6d7f5df03be4f0c6301532ec
bestoon <148741679+vestoon@users.noreply.github.com>
2024-11-04 19:20:47 +0900

    Title: 최소 경로로 탈출 하기, Time: 79ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241104/최소 경로로 탈출 하기/escape-with-min-distance.py
@@ -0,0 +1,33 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+dy = (1, -1, 0, 0)
+dx = (0, 0, 1, -1)
+
+n, m = map(int, input().split())
+grid = list(map(int, input().split()))
+
+def bfs(i, j):
+    que = deque()
+    que.append((i, j))
+    visited = [[False for x in range(m)] for y in range(n)]
+    visited[i][j] = True
+    ret = 0
+
+    while que:
+        ret += 1
+        l = len(que)
+        for _ in range(l):
+            cur_i, cur_j = que.popleft()
+            if cur_i == n-1 and cur_j == m-1:
+                return ret
+
+            for d in range(4):
+                nxt_i, nxt_j = cur_i+dx[d], cur_j+dy[d]
+
+                if 0<=nxt_i<n and 0<=nxt_j<m and not visited[nxt_i][nxt_j] and grid[nxt_i][nxt_j]:
+                    que.append((nxt_i, nxt_j))
+                    visited[nxt_i][nxt_j] = True
+
+print(bfs(0, 0))
