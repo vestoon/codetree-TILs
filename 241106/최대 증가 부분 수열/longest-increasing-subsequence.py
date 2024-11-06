f06a6158e82d53c6e9c849a6be6715d988bf27f7
bestoon <148741679+vestoon@users.noreply.github.com>
2024-11-06 14:04:43 +0900

    Title: 최대 증가 부분 수열, Time: 73ms, Memory: 16MB, Status: Passed - CodeTree

--- 241106/최대 증가 부분 수열/longest-increasing-subsequence.py
+++ 241106/최대 증가 부분 수열/longest-increasing-subsequence.py
@@ -4,7 +4,7 @@ input = sys.stdin.readline
 N = int(input())
 arr = list(map(int, input().split()))
 
-dp = [0 for x in range(N)]
+dp = [1 for x in range(N)]
 dp[0] = 1
 for j in range(1, N):
     for i in range(j):
