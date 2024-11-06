b72149ab11128f2472e7c476899d46f27a4b329e
bestoon <148741679+vestoon@users.noreply.github.com>
2024-11-06 11:10:53 +0900

    Title: 비를 피하기, Time: 247ms, Memory: 24MB, Status: Passed - CodeTree

--- 241106/비를 피하기/stay-out-of-rain.py
+++ 241106/비를 피하기/stay-out-of-rain.py
@@ -11,30 +11,24 @@ answer = [[0 for x in range(n)] for y in range(n)]
 def bfs(i, j):
     que = deque()
     que.append((i, j))
-    visited = [[False for x in range(n)] for y in range(n)]
-    visited[i][j] = True
-    distance = 0
+    distance = [[sys.maxsize for x in range(n)] for y in range(n)]
+    distance[i][j] = 0
 
     while que:
-        l = len(que)
-        # print(que)
-        for _ in range(l):
-            cur_i, cur_j = que.popleft()
+        cur_i, cur_j = que.popleft()
+        # print(cur_i, cur_j)
 
-            if grid[cur_i][cur_j] == 3:
-                answer[i][j] = distance
-                return
+        if grid[cur_i][cur_j] == 3:
+            answer[i][j] = distance[cur_i][cur_j]
+            return
 
-            for d in range(4):
-                nxt_i, nxt_j = cur_i+dx[d], cur_j+dy[d]
+        for d in range(4):
+            nxt_i, nxt_j = cur_i+dx[d], cur_j+dy[d]
 
-                if 0<=nxt_i<n and 0<=nxt_j<n and not visited[nxt_i][nxt_j] and grid[nxt_i][nxt_j] != 1:
-                    visited[nxt_i][nxt_j] = True
-                    que.append((nxt_i, nxt_j))
+            if 0<=nxt_i<n and 0<=nxt_j<n and distance[nxt_i][nxt_j] > distance[cur_i][cur_j]+1 and grid[nxt_i][nxt_j] != 1:
+                distance[nxt_i][nxt_j] = distance[cur_i][cur_j] + 1
+                que.append((nxt_i, nxt_j))
 
-        # print(d)      
-        distance += 1
-    
     answer[i][j] = -1
     return
 
