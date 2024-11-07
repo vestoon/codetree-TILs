9934f9a46650214ad0f13038615d9ba48abe7c1f
bestoon <148741679+vestoon@users.noreply.github.com>
2024-11-07 19:58:12 +0900

    Title: 동전 거슬러주기, Time: 75ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241107/동전 거슬러주기/coin-change.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+def sol():
+    N, M = map(int, input().split())
+    coins = list(map(int, input().split()))
+    # print(coins)
+    dp = [0 for x in range(M+1)]
+    for coin in coins:
+        dp[coin] = 1
+
+
+    for i in range(M+1):
+        if not dp[i]: continue
+
+        for coin in coins:
+            if i+coin > M: continue
+            # print(i, coin)
+            dp[i+coin] = min(dp[i+coin], dp[i]+1) if dp[i+coin] else dp[i]+1
+        
+        # print(i, dp)
+    
+    print(dp[M] if dp[M] else -1)
+
+sol()
