5b26151153702c53cc301a6c62817aa75a9f1cca
bestoon <148741679+vestoon@users.noreply.github.com>
2024-11-08 17:34:08 +0900

    Title: 올바른 등식 만들기, Time: 65ms, Memory: 16MB, Status: Passed - CodeTree

--- 241108/올바른 등식 만들기/right-equality.py
+++ 241108/올바른 등식 만들기/right-equality.py
@@ -11,6 +11,8 @@ for a in arr:
     tmp = [0 for x in range(41)]
 
     for i in range(-20, 21):
+        if dp[i] <= 0: continue
+
         if i+a <= 20:
             tmp[i+a] += dp[i]
         if -20 <= i-a:
