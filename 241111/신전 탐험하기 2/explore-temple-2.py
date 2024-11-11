2fd236730fef748e1354c859bd1c80bb765d1c7f
bestoon <148741679+vestoon@users.noreply.github.com>
2024-11-11 16:27:32 +0900

    Title: 신전 탐험하기 2, Time: 110ms, Memory: 18MB, Status: Passed - CodeTree

--- 241111/신전 탐험하기 2/explore-temple-2.py
+++ 241111/신전 탐험하기 2/explore-temple-2.py
@@ -8,7 +8,7 @@ n 층으로 이루어진 신전, 층마다 방 3개 하나만 갈 수 있음
 
 꼭대기와 1층의 방의 종류가 같으면 안된다. 
 
-dp[i][j][k] : [i] 출발한 층 수 [j] 현재 층 수 [k] 현재 층으 방 종류
+dp[i][j][k] : [i] 1층에서 선택한 방의 종류 [j] 현재 층 수 [k] 현재 층으 방 종류
 """
 
 n = int(input())
@@ -19,13 +19,12 @@ for _ in range(n):
     l, m, r = map(int, input().split())
     temple.append((l, m, r))
 
-for i in range(3): # i층에서 출발했을 때
-    for k in range(3):
-        dp[i][0][k] = temple[0][k]
+for i in range(3): # 1 층에서 i 종류의 방을 선택했을 때 
+    dp[i][0][i] = temple[0][i]
     
     for j in range(1, n):
-        for k in range(3):
-            for pk in range(3):
+        for k in range(3): # 현재 선택한 방의 종류
+            for pk in range(3): # 이전에 선택한 방의 종류
                 if k == pk: continue
 
                 dp[i][j][k] = max(dp[i][j][k], dp[i][j-1][pk]+temple[j][k])
